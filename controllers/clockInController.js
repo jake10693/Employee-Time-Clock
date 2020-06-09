a4fb4683f6e99d0e0e08ed0c0030d6c7d2e9eed9
Yeasir H <yeasir01@gmail.com>
2020-06-09 00:38:39 -0700

    clean branch

--- controllers/clockInController.js
+++
@@ -1,9 +0,0 @@
-const db = require('../models');
-
-module.exports = {
-    insertRecord: function(req, res){
-        db.ClockIn.create(req.body)
-        .then(response => res.json(response))
-        .catch(err => res.status(422).json(err))
-    }
-}
--- routes/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/clockInController');
-
-router.route("/api/clockin")
-    .post(controller.insertRecord)
-
-module.exports = router;
