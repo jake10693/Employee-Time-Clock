ef87822951f209bc48a7fb127625cbab2a94ef07
Yeasir H <yeasir01@gmail.com>
2020-06-12 15:17:43 -0700

    built employee clock in list

--- client/src/pages/Kiosk/index.js
+++ client/src/pages/Kiosk/index.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import Box from "@material-ui/core/Box";
 import Grid from '@material-ui/core/Grid';
 import './style.css';
 import DigitalClock from '../../components/DigitialClock'
+import ClockInList from '../../components/ClockInList'
 
 function Kiosk(){
     return(
         <Grid container>
             
             <Grid item xs={false} sm={6} md={6} className="clock-grid grid">
-                <Box>
-                    <DigitalClock />
-                </Box>
+                <DigitalClock />
             </Grid>
 
             <Grid item xs={12} sm={6} md={6} elevation={6} className="grid">
-               <Link to="/timeclock"><h1>-->John Doe (TESTING)</h1></Link>
+                <ClockInList />
             </Grid>
         </Grid>
     )
 }
 
 export default Kiosk;
+
+//<Link to="/timeclock"><h1>-->John Doe (TESTING)</h1></Link>
