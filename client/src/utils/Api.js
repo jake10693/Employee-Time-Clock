5b26155e68a755953ff508d37a32396b40b0c481
Jacob Carver <jake10693@gmail.com>
2020-06-13 13:42:20 -0700

    added prospective route to axios api

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -12,5 +12,8 @@ export default {
   },
   verifyLocation: (id) => {
     return axios.get(`/api/location/${id}`)
-  }
+  },
+  getAllLocations: (id) => {
+    return axios.get(`/api/location/${id}`)
+  },
 };
