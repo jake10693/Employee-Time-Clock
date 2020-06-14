236b13be4abe77fb28096c01f445af7865e11689
Yeasir H <yeasir01@gmail.com>
2020-06-14 12:16:05 -0700

    changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-//import NumPad from "../src/components/NumPad"
 
 ReactDOM.render(<App />, document.getElementById("root"));
