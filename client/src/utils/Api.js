167cdd4e1b9b28102b08ec482d0e22df5ef57127
Yeasir H <57420487+yeasir01@users.noreply.github.com>
2020-06-16 21:43:27 -0700

    update

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -5,7 +5,7 @@ export default {
     return axios.post("/api/auth/register", data);
   },
   authenticateUser: (data) => {
-    return axios.post("/api/auth/", data);
+    return axios.post("/api/auth", data);
   },
   getUserData: (data, config) =>{
     return axios.post("/api/auth/user", data, config)
